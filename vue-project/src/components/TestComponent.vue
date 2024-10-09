166118b2c9176218410862c09358e1e210c6ce50
dominik.halvonik <dominik.halvonik@gmail.com>
2024-10-09 16:13:45 +0200

    Pridanie komponentu

---
+++ vue-project/src/components/TestComponent.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "TestComponent",
+  data() {
+    return {
+      cislo: 1,
+      meno: "Dominik",
+      priezvisko: "Martak"
+    }
+  },
+  methods: {
+    increment() {
+      this.cislo++
+    },
+    decrement() {
+      this.cislo--
+    }
+  },
+  computed: {
+    getColor() {
+      if(this.cislo < 5) {
+        return "green"
+      } else {
+        return "blue"
+      }
+    },
+    getFullName() {
+      return this.meno + " " + this.priezvisko
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p>Ahoj toto je moj prvy komponent</p>
+    <p>{{ meno }} {{ priezvisko }}</p>
+    <p>{{ getFullName }}</p>
+    <p :class="getColor">{{ getFullName }}</p>
+    <button @click="increment">Pridaj</button>
+    <button @click="decrement">Uber</button>
+  </div>
+</template>
+
+<style scoped>
+.green {
+  color: green;
+  font-size: 30px;
+}
+
+.blue {
+  color: blue;
+  font-size: 50px;
+}
+</style>
+
