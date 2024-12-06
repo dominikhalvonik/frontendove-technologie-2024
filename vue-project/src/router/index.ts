e18ff46e7941a02bd7b78b82af66a3282a8350e6
dominik.halvonik <dominik.halvonik@gmail.com>
2024-12-06 09:37:24 +0100

    Pinia 1

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import OurView from "@/views/OurView.vue";
 import AView from "@/views/AView.vue";
 import BView from "@/views/BView.vue";
+import CounterView from "@/views/CounterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,11 @@ const router = createRouter({
           props: true
         }
       ]
+    },
+    {
+      path: '/counter',
+      name: 'counter',
+      component: CounterView
     },
   ]
 })
